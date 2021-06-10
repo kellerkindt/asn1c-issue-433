11bf54417eb48d2b277508c3db7151ccda9c9593
Michael Watzko <michael@watzko.de>
2021-06-10 11:32:30 +0200

    Properly use subshell in build_asn1c_versions.sh

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-rm -rf target && \
+(\
+    rm -rf target && \
     mkdir target && \
     cd target && \
     cmake .. && \
     make -j$(nproc) && \
-    ./asn1c_sequence_of_extensible_defect
+    ./asn1c_sequence_of_extensible_defect \
+)
--- build_asn1c_versions.sh
+++ build_asn1c_versions.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-cd asn1c && $(test -f configure || autoreconf -iv) && ./configure && make -j$(nproc) && \
-cd .. && \
-cd riebl_asn1c && $(test -f configure || autoreconf -iv) && ./configure && make -j$(nproc) && \
-cd .. && \
-cd mouse07410_asn1c && $(test -f configure || autoreconf -iv) && ./configure && make -j$(nproc) && \
-cd ..
-
+(cd asn1c && (test -f configure || autoreconf -iv) && ./configure && make -j$(nproc)) && \
+(cd riebl_asn1c && (test -f configure || autoreconf -iv) && ./configure && make -j$(nproc)) && \
+(cd mouse07410_asn1c && (test -f configure || autoreconf -iv) && ./configure && make -j$(nproc))
